e577eff84fa6dd5d427c7042c71262f3265982c0
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-21 10:54:50 +0530

    postgres

--- chat/wsgi.py
+++ chat/wsgi.py
@@ -1,7 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
+application = Cling(get_wsgi_application())
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
