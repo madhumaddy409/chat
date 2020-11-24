d3bc09b6980a1e25672b689d5e6d7216f8bc11c1
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-24 11:17:56 +0530

    redis

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,7 +1,11 @@
-import os
-import django
-from channels.routing import get_default_application
+# import os
+# import django
+# from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
-django.setup()
-application = get_default_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
+# django.setup()
+# application = get_default_application()
+import os
+import channels 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings_production")
+channel_layer = channels.asgi.get_channel_layer()
