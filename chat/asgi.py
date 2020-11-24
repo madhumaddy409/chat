14e51ccaa497f99addc04a51027e2b7bc189152c
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-24 12:09:55 +0530

    asgi

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,7 +1,19 @@
-import os
-import django
-from channels.routing import get_default_application
+# import os
+# import django
+# from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
-django.setup()
-application = get_default_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
+# django.setup()
+# application = get_default_application()
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+})
