3535307751c83b1dcc1c4cfad975f3ceb63874a7
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-24 12:03:22 +0530

    asgi

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne routeradmin.asgi:apllication --port $PORT --bind 0.0.0.0 -v2
-chatworker: python manage.py runserver runworker --settings=routeradmin.settings -v2
+web: daphne WR.asgi:apllication --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runserver runworker --settings=WR.settings -v2
--- chat/asgi.py
+++ chat/asgi.py
@@ -1,13 +1,7 @@
-# import os
-# import django
-# from channels.routing import get_default_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
-# django.setup()
-# application = get_default_application()
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "routeradmin.settings_production")
+import django
+from channels.routing import get_default_application
 
-from channels.asgi import get_channel_layer
-
-channel_layer = get_channel_layer()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
+django.setup()
+application = get_default_application()
