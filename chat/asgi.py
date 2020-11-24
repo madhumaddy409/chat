534f8eabead8893f15212324622b2ed352edba26
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-24 11:38:05 +0530

    asgi

--- chat/asgi.py
+++ chat/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings_production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","chat.settings")
 django.setup()
 application = get_default_application()
