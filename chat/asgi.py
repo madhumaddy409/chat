85211fa740d5f4caed32edb6e0d9ca54dddeb4b5
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-11-24 11:08:53 +0530

    redis

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,9 +1,10 @@
 import os
-import django
-from channels.routing import get_default_application
+
+from django.core.asgi import get_asgi_application
+from dj_static import Cling
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
-django.setup()
-# application = get_wsgi_application()
-application = get_default_application()
+
+
+application = get_asgi_application()
